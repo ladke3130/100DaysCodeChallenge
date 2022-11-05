7e36a2818e1ec5d5427ddab0a7a1ad529c083692
ladke3130 <117341099+ladke3130@users.noreply.github.com>
2022-11-05 08:40:12 -0700

    Create Reverse.java

---
+++ Day-2/ReverseANumber/Reverse.java
@@ -0,0 +1,26 @@
+//Test case1:
+Input:
+Enter any number:123
+Output:321
+//Test Case2:
+Input:
+Enter any number:98705
+Output:50789
+  
+....................................................................................................................................................................
+import java.util.Scanner ;
+public class Reverse {
+    public static void main(String[] args) {
+        int no , rev=0,r,a;
+        Scanner sc = new Scanner (System. in);
+        System.out.print("enter any number : ");
+        no = sc.nextInt();
+        a= no ;
+        while(no >0) {
+            r = no %10;
+            rev = rev*10+r;
+            no = no /10;
+        }
+        System.out.println("Reverse : " + rev);
+    }
+    }
