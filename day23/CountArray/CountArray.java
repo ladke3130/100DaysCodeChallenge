9de9795b15d59fe947e3885c2f8cda3d62ccad80
ladke3130 <117341099+ladke3130@users.noreply.github.com>
2022-11-27 10:13:39 -0800

    Create CountArray.java

---
+++ day23/CountArray/CountArray.java
@@ -0,0 +1,15 @@
+//Definition :-In this program, we need to count and print the number of elements present in the array.
+
+//The number of elements present in the array can be found by calculating the length of the array.
+//Test case :- 
+Output :- 
+  Number of elements present in given array: 5
+
+public class CountArray {  
+    public static void main(String[] args) {  
+      //Initialize array  
+        int [] arr = new int [] {1, 2, 3, 4, 5};  
+      //Number of elements present in an array can be found using the length  
+        System.out.println("Number of elements present in given array: " + arr.length);  
+    }  
+} 
