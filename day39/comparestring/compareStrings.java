6222484758fc0925d106664e85030f11c1e7f12a
ladke3130 <117341099+ladke3130@users.noreply.github.com>
2023-01-18 20:42:33 -0800

    Create compareStrings.java

---
+++ day39/comparestring/compareStrings.java
@@ -0,0 +1,21 @@
+/*Definition :- this program, you'll learn to compare two strings in Java.
+
+To understand this example, you should have the knowledge of the following Java programming topics:
+
+Test case 1 :
+input :- strng 1 :- Bold
+         string 2:- Gold
+ output :- not equal*/
+public class CompareStrings {
+
+    public static void main(String[] args) {
+
+        String style = new String("Bold");
+        String style2 = new String("Bold");
+
+        if(style.equals(style2))
+            System.out.println("Equal");
+        else
+            System.out.println("Not Equal");
+    }
+}
