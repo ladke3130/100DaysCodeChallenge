8cc1bffcd4c876000c9f894be1870a1626a67cfe
nikita ladke <117341099+ladke3130@users.noreply.github.com>
2023-03-20 21:06:30 +0530

    Create ArrayAvarage.java

---
+++ day-59/AvarageArray/ArrayAvarage.java
@@ -0,0 +1,30 @@
+//Definition :-To calculate the average of numbers given in the Java array, first, 
+/*we need to calculate the sum and then find the average using sum / number_of_array_elements.
+  Test case :- 
+    input :-
+      array[] = {10, 20, 30, 40, 50};
+output:-
+then sum of array elements = 10 + 20 + 30 + 40 + 50 = 150
+and the average of numbers = 150 / 5 = 30*/
+public class ArrayAverage {
+  public static void main(String[] args) {
+
+    double array[] = {10, 20, 30, 40, 50};
+    // declare sum variable, & initialize with 0
+    double sum = 0.0;
+    // declare average variable
+    double avg = 0.0;
+
+    // loop to iterate the array
+    for (int i=0; i<array.length; i++) {
+      // add numbers
+      sum = sum + array[i];
+    }
+
+    // calculate the average value
+    avg = sum/array.length;
+
+    // display result
+    System.out.println("Average: " + avg );
+  }
+}
