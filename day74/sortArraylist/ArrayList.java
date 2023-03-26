7a9d3f2576cf0014620782c177c65a05e860a577
nikita ladke <117341099+ladke3130@users.noreply.github.com>
2023-03-26 15:53:04 +0530

    Create ArrayList.java

---
+++ day74/sortArraylist/ArrayList.java
@@ -0,0 +1,36 @@
+/*Definition :-An ArrayList can be Sorted by using the sort() method of the Collections Class in Java.
+  This sort() method takes the collection to be sorted as the parameter and returns a Collection sorted in the Ascending Order by default.
+  Test case :-Unsorted ArrayList: [Geeks, For, ForGeeks, GeeksForGeeks, A computer portal]
+Sorted ArrayList in Ascending order : [A computer portal, For, ForGeeks, Geeks, GeeksForGeeks]*/
+
+import java.util.*;
+
+public class ArrayList {
+	public static void main(String args[])
+	{
+
+		// Get the ArrayList
+		ArrayList<String>
+			list = new ArrayList<String>();
+
+		// Populate the ArrayList
+		list.add("Geeks");
+		list.add("For");
+		list.add("ForGeeks");
+		list.add("GeeksForGeeks");
+		list.add("A computer portal");
+
+		// Print the unsorted ArrayList
+		System.out.println("Unsorted ArrayList: "
+						+ list);
+
+		// Sorting ArrayList in ascending Order
+		// using Collection.sort() method
+		Collections.sort(list);
+
+		// Print the sorted ArrayList
+		System.out.println("Sorted ArrayList "
+						+ "in Ascending order : "
+						+ list);
+	}
+}
