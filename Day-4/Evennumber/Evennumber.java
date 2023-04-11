203d79e45cb1a674a199d2264ecf3ab8cabd1a0d
nikita ladke <117341099+ladke3130@users.noreply.github.com>
2023-04-11 15:24:41 +0530

    Update Evennumber.java

--- Day-4/Evennumber/Evennumber.java
+++ Day-4/Evennumber/Evennumber.java
@@ -1,10 +1,10 @@
-//Defination :- this list even number java example shows how to find and list even number between 1 to any given number .
+/*//Defination :- this list even number java example shows how to find and list even number between 1 to any given number .
 //Test case 1 : - 
   input :- printting even number between 1 to 20
     output :- 2 4 6 8 10 12 14 16 18 20
  // Test case 2 :- 
       input :- printting even number between 1 to 10
-        output :- 2 4 6 8 10
+        output :- 2 4 6 8 10*/
           
     public class Evennumber {
     public static void main(String[] args) {
